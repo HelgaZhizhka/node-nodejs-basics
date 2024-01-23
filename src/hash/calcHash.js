1b965412190993b907d06bd8fed02a7ef2d9fbd2
Olga Zhyzhka <helgazhyzhka@gmail.com>
2024-01-23 19:57:32 +0100

    feat: implement SHA256 hash crypto task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises'
+import crypto from 'crypto'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await calculateHash();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const fileToHash = join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+const hash = crypto.createHash('sha256')
+
+const calculateHash = async () => {
+  const stream = fs.createReadStream(fileToHash)
+  stream.on('data', (data) => hash.update(data))
+  stream.on('end', () => {
+    const result = hash.digest('hex')
+    console.log(result)
+  })
+}
+
+await calculateHash()
