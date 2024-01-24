e1a1248649258be4687f4d125e9c98bf6987c989
Olga Zhyzhka <helgazhyzhka@gmail.com>
2024-01-24 20:21:51 +0100

    feat: impement stream task: read and write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await read();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const fileToRead = join(__dirname, 'files', 'fileToRead.txt')
+
+const read = async () => {
+  const stream = fs.createReadStream(fileToRead)
+  stream.on('data', (data) => console.log(data.toString()))
+  stream.on('error', (err) => console.error(err.message))
+  stream.on('end', () => console.log('File read successfully'))
+}
+
+await read()
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
-const write = async () => {
-    // Write your code here 
-};
+import fs from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await write();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const fileToWrite = join(__dirname, 'files', 'fileToWrite.txt')
+
+const write = async () => {
+  const writeStream = fs.createWriteStream(fileToWrite)
+  process.stdin.on('data', (data) => {
+    writeStream.write(data)
+  })
+  writeStream.on('finish', () => {
+    console.log('File written successfully')
+  })
+  writeStream.on('error', (error) => {
+    console.error('Error writing file:', error)
+  })
+
+  process.stdin.on('error', (error) => {
+    console.error('Error reading input:', error)
+  })
+  process.on('SIGINT', () => {
+    writeStream.end()
+    process.exit()
+  })
+}
+
+await write()
