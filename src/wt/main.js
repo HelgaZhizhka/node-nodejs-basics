c6cf940657af699faf51715108291fe99ef43907
Olga Zhyzhka <helgazhyzhka@gmail.com>
2024-01-26 19:35:29 +0100

    feat: add task work-threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'node:worker_threads'
+import os from 'node:os'
 
-await performCalculations();
+const performCalculations = async () => {
+  const numberOfCores = os.cpus().length
+  const promises = []
+
+  for (let i = 0; i < numberOfCores; i++) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker('./src/wt/worker.js')
+        worker.postMessage(10 + i)
+
+        worker.on('message', (result) => {
+          resolve({ status: 'resolved', data: result })
+        })
+
+        worker.on('error', reject)
+        worker.on('exit', (code) => {
+          if (code !== 0)
+            reject(new Error(`Worker stopped with exit code ${code}`))
+          process.exit(code)
+        })
+      })
+    )
+  }
+
+  const results = await Promise.allSettled(promises)
+  console.log(results)
+}
+
+await performCalculations()
